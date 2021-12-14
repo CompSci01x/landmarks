2e64cb21eb50b8f1e9c396704e2fc70fc4b46445
CompSci01x <42660883+CompSci01x@users.noreply.github.com>
2021-12-13 19:26:27 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by gkruchinin on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
